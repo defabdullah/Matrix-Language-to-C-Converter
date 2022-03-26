45a56fe339026bcc49807a36c3b7a5a448613c15
BatuhanIlhan <mbatuhanilhan@gmail.com>
2022-03-26 16:31:33 +0300

    parsing keyword

---
+++ file.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+
+void printsep(){ 
+	printf("----------\n"); 
+}
+
+int main(){
+
+	double count ;
+	double incr ;
+	double B[4][1] ;
+	double i ;
+	double j ;
--- main.c
+++ main.c
@@ -2,6 +2,51 @@
 #include <string.h>
 #include <ctype.h>
 
+enum types{scalar,vector,matrix};
+
+void print_usual(FILE *pfnew){
+    fprintf(pfnew, "%s","#include <stdio.h>\n#include <string.h>\n#include <ctype.h>\n\n");
+    fprintf(pfnew,"%s","\nvoid printsep(){ \n\tprintf(\"----------\\n\"); \n}");
+    fprintf(pfnew,"%s","\n\nint main(){\n\n");
+
+}
+
+void scalar_line(FILE* pfnew,char* line, int linenumber){
+    char * token;
+    while ((token=strsep(&line," "))!=NULL ){
+        if(strcmp(token,"")==0){
+                continue;
+        }
+        fprintf(pfnew,"%s %s %s","\tdouble",token,";\n");
+
+    }
+}
+
+void matrix_line(FILE* pfnew,char* line, int linenumber){
+    char * token;
+    while ((token=strsep(&line," "))!=NULL ){
+        if(strcmp(token,"")==0){
+                continue;
+        }
+        fprintf(pfnew,"%s %s %s","\tdouble",token,";\n");
+    }
+}
+
+
+void vector_line(FILE* pfnew,char* line, int linenumber){
+    fprintf(pfnew,"%s","\tdouble ");
+    char * token;
+    while ((token=strsep(&line," "))!=NULL ){
+        if(strcmp(token,"")==0){
+                continue;
+        }
+        fprintf(pfnew,"%s", token);
+    }
+    fprintf(pfnew,"%s","[1] ;\n");
+
+}
+
+
 int is_alphanumeric(char* s){
 
     if((*s>='A' && *s<='Z') || (*s>='0' && *s <= '9') || (*s>='a' && *s<='z')){
@@ -33,10 +78,12 @@ char *trim(char *s)
 
 
 int main(int argc,char *argv[]){
+    FILE *pfnew;
     FILE *fp;
 
     char line[256];
-  
+    pfnew=fopen("file.c","w");
+    print_usual(pfnew);
     if (argc != 2) {
     printf("Give filename as command line argument\n") ;
         fp = fopen(argv[1], "r");
@@ -48,11 +95,14 @@ int main(int argc,char *argv[]){
         return 1;
     }
 
-    int currentLine=0;
+    int linenumber=0;
     //char extended[512];
     char * token;
+    int is_declaratiion=1;
+    int is_infor=0;
+    char * for_state;
     while( fgets(line,256,fp) != NULL ) {
-        currentLine++;
+        linenumber++;
         
         // checks if line starts with '#' or line is an empty line, if so continues with next line
         if(line[0]=='#' || line[0]== '\n'){
@@ -84,15 +134,59 @@ int main(int argc,char *argv[]){
             cur++;
         }
 
-        char * pextended = strdup(extended);
+        char * pextended = strdup(trim(extended));
+        char * pextendeds = strdup(trim(extended));
+        // declaration while
+        while ( is_declaratiion==1 && (token=strsep(&pextended," "))!=NULL ){
+            if(strcmp(token,"")==0){
+                continue;
+            }
+            if (strcmp("scalar",token)==0){
+                scalar_line(pfnew,pextended,linenumber);
+                break;
+            }else if(strcmp("vector",token)==0){
+                vector_line(pfnew,pextended,linenumber);
+                break;
+            }else if(strcmp("matrix",token)==0){
+                /*matrix_line(pfnew,pextended,linenumber);*/
+                break;
+            }else{
+                is_declaratiion=0;
+            }
+        }
+        //statement line
+        while((token=strsep(&pextendeds," "))!=NULL && is_declaratiion==0){
+            if(strcmp(token,"")==0){
+                continue;
+            }
+            if (strcmp(token,"}")==0){
+                if(is_infor==1){
+                    is_infor=0;
+                    break;
+                }
+            }
+            else if(is_infor==1){
+                //print to other file
+                break;
+            }
+            else if(strcmp(token,"for")==0){
+                is_infor=1;
+                //for function;
+                break;
+            }else if(strcmp(token,"print")==0){
+                // print func
+                break;
+            }else if(strcmp(token,"printsep")==0){
+                // print func
+                break;
+            }else{
+                //asign statement
+                break;
+            }
 
-        /*while ((token=strsep(&pextended," "))!=NULL){
-             printf("%s \n",token);
-        }*/
-       
 
+        }
 
-        printf("%s \n",trim(extended));
         memset(extended, 0, 256);
     }
 
