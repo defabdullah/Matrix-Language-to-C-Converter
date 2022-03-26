f09b2a57f884a8d719247a39a9f69bf394bc1bea
ahmet abdullah susuz <abdullah.susuz@twiser.com>
2022-03-26 12:08:18 +0300

    correct transpose and matrix*scalar 2-d array

--- transpose/transpose_template.c
+++ transpose/transpose_template.c
@@ -1,35 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main() {
-    double scalarTranspose(double num1){
+double scalarTranspose(double num1){
         return num1;
     }
 
-    double *matrixTranspose(double *matrix, int row, int column){
-        double* newMatrix;
-        newMatrix=malloc(sizeof(double*)*row*column);
-        for(int i=0;i<column;i++){
-            for(int j=0;j<row;j++){
-                *(newMatrix+i*row+j) = *(matrix+(j*column+i));
-            }
-        }
-        return newMatrix;
+double **matrixTranspose(int row,int column,double **matrix){
+    double **newMatrix;
+    newMatrix=(double**)calloc(column,sizeof(double*));
+    for(int i=0;i<column;i++){
+        newMatrix[i]=(double*)calloc(row,sizeof(double));
     }
+
+    for(int i=0;i<column;i++){
+        for(int j=0;j<row;j++){
+            newMatrix[i][j]=matrix[j][i];
+        }
+    }
+    return newMatrix;
+}
+
+void printMatrix(int row,int column,double **matrix){
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            printf("%f\n",matrix[i][j]);
+        }
+    }
+}
+
+int main() {
     
-    void printMatrix(double *matrix,int row,int column){
-        for(int i=0;i<row*column;i++){
-            printf("%f\n",*(matrix+i));
+    int row=3;
+    int column=4;
+
+    //matrix definition with pointer
+    double **firstMatrix;
+    firstMatrix=(double**) calloc(row,sizeof(double*));
+    for(int i=0;i<row;i++){
+        firstMatrix[i]=(double*)calloc(column,sizeof(double*));
+    }
+
+    double array[3][4]={{1.0,2.0,3.0,4.0},{5.0,6.0,7.0,8.0},{9.0,10.0,11.0,12.0}};
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            firstMatrix[i][j]=array[i][j];
         }
     }
 
-    int row=4;
-    int column=3;
-
-    double array[4][3]={{2.4,1.42,3.214},{5.325,34.153,158.12},{26.214,125.12,23.32},{632214.32,94.14,2.1}};
-    
-    double *newmatrix=malloc(sizeof(double*)*row*column);
-    newmatrix=matrixTranspose((&array)[0][0],row,column);
-    printMatrix(newmatrix,row,column);
+    printMatrix(row,column,firstMatrix);
+    printf("----------------\n");
+    //call matrix transpose func
+    double **secondMatrix=matrixTranspose(row,column,firstMatrix);
+    //call print matrix function (reverse row-column)
+    printMatrix(column,row,secondMatrix);
     
    return 0;
 }
